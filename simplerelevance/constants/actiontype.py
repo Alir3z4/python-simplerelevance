13c38d7395f6c53a0845c52b3ba85e12e8bc7c7e
Alireza Savand <ali@simplerelevance.com>
2013-04-12 01:34:57 +0400

    Added constants module

---
+++ simplerelevance/constants/actiontype.py
@@ -0,0 +1,14 @@
+class ActionTypes:
+    """
+    All actions are a combination of a user and an item.
+    These include:
+
+    - Purchases:    (action type 1)
+    - Clicks:       (action type 0)
+    - Email opens:  (action type 5)
+
+    [with more to come.]
+    """
+    PURCHASES = 1
+    CLICKS = 0
+    EMAIL_OPENS = 5
