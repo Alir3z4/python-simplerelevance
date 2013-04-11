40fdaf462eb25d21168655463abf56ae326d712d
Alireza Savand <ali@simplerelevance.com>
2013-04-11 21:48:36 +0400

    Added pair_required

--- simplerelevance/utils.py
+++ simplerelevance/utils.py
@@ -5,4 +5,10 @@ def arg_name(arg):
     return inspect.getargspec(arg)[0]
 
 
+def pair_required(first, second):
+    if (first or second) and (not first or not second):
+            raise ValueError(
+                "'%s', '%s' required to both be provided" % (arg_name(first),
+                                                             arg_name(second))
+            )
 
