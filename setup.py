ed2352113e025342537f7ce6f7df8b9f96486484
Alireza Savand <ali@simplerelevance.com>
2013-04-19 21:12:35 +0400

    Added setup.py file and bumb the version

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='python-simplerelevance',
+    version='0.1',
+    packages=['simplerelevance', 'simplerelevance.constants'],
+    url='https://github.com/SimpleRelevance/python-simplerelevance',
+    license=open('LICENSE').read(),
+    author='Alireza Savand',
+    author_email='alireza.savand@gmail.com',
+    description='SimpleRelevance API Python Wrapper',
+    long_description=open('README.rst').read()
+)
--- simplerelevance/__init__.py
+++ simplerelevance/__init__.py
@@ -1,2 +1,2 @@
-__VERSION__     = (0,0)
+__VERSION__ = (0, 1)
 __API_VERSION__ = 3
